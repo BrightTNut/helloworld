47cfeeadef746554b955c3e27b91ed52b5c0b0b0
BrightTNut <118769123+BrightTNut@users.noreply.github.com>
2024-08-11 12:59:13 +0530

    primative data types

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    println!("Hello, world!");
+
+
+//i = sign integer , u = unsign integer
+let x : i8 = -90;
+let y : u8 = 90;
+println!("Sign Integer: {}",x);
+println!("UnSign Integer: {}",y);
+
+let f : f64 = 3.21;
+println!("Float Integer: {}",f);
+
+let b : bool = true;
+println!("Bool data Type: {}",b);
+
+let letter : char = 'a';
+println!("Char data Type: {}",letter);
+
+let strings : String = "tejas".to_string();
+println!("String data Type: {}",strings);
+
+
+}
