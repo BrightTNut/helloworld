765a1ef6321abc27a4d35b007a205fe0ae2e8fba
BrightTNut <118769123+BrightTNut@users.noreply.github.com>
2024-08-12 11:49:31 +0530

    ownerhsip lesson

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,35 @@
 fn main() {
     println!("Hello, world!");
 
-    //function and / variavle should be written in snake case
-    //snake case : name_with_underline = hello_world
-    //kebab case : hello-world
+    //Ownership 
+    //Ownership : every value has a single owner 
+    //every variable has one value, and it is its sole owner
+    //Ownership intreduce in Rust to resolve memory safety issues and performance at same time
+    
+//     Ownership Rules
+//     Each value in Rust has an owner.
+//     There can only be one owner at a time.
+//     When the owner goes out of scope, the value will be dropped.
 
-    hello_world();
-    tell_height(180);
-    human_id("Tejas".to_string(),21, 180.7);
+    //example on each rule of ownership
+    //1  Each value in Rust has an owner.
+    let s1= String ::from("Rust");
+    let len  = calculate_len(&s1);
+    print!("Length of {s1} is : {len}");
 
-    let x : i8= {
-        let price : i8 = 5;
-        let qty : i8 = 10;
-        //default return is present at the end of expression in Rust
-        price * qty
-    };
-    print!("\nResult of Expression : {}",x);
-    //add function
-    print!("\n Result of 'add' Function: {}." , add(7,7));
+//  2.   There can only be one owner at a time.
+    print!("\nBefor :owner of {s1} is 's1' now");
+    let s2 = s1;
+    print!("\nAfter :owner of {s2} is 's2' now");
 
-    //bmi function call
-    let weight : f64 = 64.7;
-    let height : f64 = 1.67;
-    let bmi : f64 = calculate_bmi(weight, height);
-    //{:.2} for allow second decimal after point 
-    print!("\nBMI of your Body : {:.2}. ",bmi);
 }
 
-//Hoisiting - calling function anywhere in the code
-fn hello_world(){
-    print!("In Hello World Function !!!\n")
+//  3.   When the owner goes out of scope, the value will be dropped.
+fn print_out_side(){
+    print!("Out side of scope value of s2 is {s1}");
 }
 
-fn tell_height(height: u8){
-    print!("Your Height is : {} cm",height);
+fn calculate_len(s: &String)->usize{
+  s.len()
 }
-
-fn human_id(name :String,age:u8,height:f32){
-    print!("\nMy name is {}, My age is {}, My height is {}",name,age,height);
-}
-
-fn add(a:i8,b:i8)-> i8{
-    return a + b;
-}
-//Expression and Statments
-//Expression : Anything that returns Value
-//Statments : Anything that not return any value
-
-//statements
-//almost all statements in Rust end with ; .
-//let y = let x= 10;
-//1 variable decrations : let x =5;
-//2 function definitions : fn foo(){}
-//3 control flow statements (loops)
-
-//final example 
-//BMI = kg/cm*cm
-
-fn calculate_bmi(weight_kg: f64,height_m:f64)-> f64{
-     weight_kg / (height_m * height_m)
-}
+    
