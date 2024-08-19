45de0a0d6a07aa161694998ca4a166e86df125bd
BrightTNut <118769123+BrightTNut@users.noreply.github.com>
2024-08-19 18:20:32 +0530

    learn struct and impl in rust

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,35 @@
 fn main() {
     println!("Hello, world!");
+//Borrowing and Refrences
+let mut _x = 5;
+let _r = &mut _x;
+// for doing arthemadic opration
+*_r += 2;
+println!("Value of _x after referancing : {_r}");
 
-    //Ownership 
-    //Ownership : every value has a single owner 
-    //every variable has one value, and it is its sole owner
-    //Ownership intreduce in Rust to resolve memory safety issues and performance at same time
-    
-//     Ownership Rules
-//     Each value in Rust has an owner.
-//     There can only be one owner at a time.
-//     When the owner goes out of scope, the value will be dropped.
-
-    //example on each rule of ownership
-    //1  Each value in Rust has an owner.
-    let s1= String ::from("Rust");
-    let len  = calculate_len(&s1);
-    print!("Length of {s1} is : {len}");
-
-//  2.   There can only be one owner at a time.
-    print!("\nBefor :owner of {s1} is 's1' now");
-    let s2 = s1;
-    print!("\nAfter :owner of {s2} is 's2' now");
+//Example
+let mut account = BankAccount{
+    owner:"Prathmesh".to_string(),
+    balance:1550.23,
+};
+//withdraw amount
+account.withdraw(50.5);
+//print out of balance
+account.print_balance();
 
 }
 
-//  3.   When the owner goes out of scope, the value will be dropped.
-fn print_out_side(){
-    print!("Out side of scope value of s2 is {s1}");
+struct BankAccount{
+    owner:String,
+    balance:f64,
 }
 
-fn calculate_len(s: &String)->usize{
-  s.len()
+impl BankAccount{
+    fn withdraw(&mut self,amount: f64){
+        println!("Withrawing {} from {} account!!.",amount,self.owner);
+        self.balance -= amount;
+    }
+    fn print_balance(&self){
+        println!("Now Balance of Account is {}",self.balance);
+    }
 }
-    
