22f16489904c3236d769e0a8775498bd13f5ecdc
BrightTNut <118769123+BrightTNut@users.noreply.github.com>
2024-08-19 19:26:28 +0530

    variable and constant

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,11 @@
 fn main() {
     println!("Hello, world!");
-//Borrowing and Refrences
-let mut _x = 5;
-let _r = &mut _x;
-// for doing arthemadic opration
-*_r += 2;
-println!("Value of _x after referancing : {_r}");
-
-//Example
-let mut account = BankAccount{
-    owner:"Prathmesh".to_string(),
-    balance:1550.23,
-};
-//withdraw amount
-account.withdraw(50.5);
-//print out of balance
-account.print_balance();
-
+//variable mut
+let mut x:i8=99;
+//constant 
+const Y:i32 = 990;
+println!("Value of x: {}",x);
+println!("Value of Constant y: {}",Y);
+println!("Value of Constant PI: {}",PI);
 }
-
-struct BankAccount{
-    owner:String,
-    balance:f64,
-}
-//struct and impl name shuld be same 
-impl BankAccount{
-    fn withdraw(&mut self,amount: f64){
-        println!("Withrawing {} from {} account!!.",amount,self.owner);
-        self.balance -= amount;
-    }
-    fn print_balance(&self){
-        println!("Now Balance of Account is {}",self.balance);
-    }
-}
+ const PI:i32 = 312;
