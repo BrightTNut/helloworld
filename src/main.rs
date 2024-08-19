2dcec0270325d02659f908cf4b4d77f23ba555ba
BrightTNut <118769123+BrightTNut@users.noreply.github.com>
2024-08-20 02:56:28 +0530

    if else

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,32 @@
+
+//For allowing warnings
+#![allow(warnings)]
+
 fn main() {
     println!("Hello, world!");
-//Shadowing
-let x=9;
-let x=x+5;
-{
-     let x= x *3;
-    println!("The value of x in inner : {}",x);
-
+//Loops control flow
+// if and else have same data type of output must be return
+// if..else
+let age:u8 = 19;
+if age>=18{
+ println!("You can drive a Class....!")
+}else{
+    println!("You can not drive a Class....!")
 }
-println!("Value of x is:{}",x);
+
+//Multiple conditions with  if..else  
+let number = 11;
+if number % 2 == 0{
+    println!("Number is Even.!");
+}else if number % 3 == 0{
+    println!("Number is Odd.!");
+}else {
+    println!("Number is Prime.!");
+}
+
+//using let in statments
+let condition = true;
+let numbers = if condition {5} else {6};
+println!("Number is {numbers}.!");
 
 }
