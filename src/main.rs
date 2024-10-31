ad3b0fa5d378ba7a1fb7a114ea0e53329a71a98e
BrightTNut <118769123+BrightTNut@users.noreply.github.com>
2024-11-01 00:03:47 +0530

    enum with code with money

--- src/main.rs
+++ src/main.rs
@@ -2,50 +2,29 @@
 //For allowing warnings
 #![allow(warnings)]
 
+
+enum CarTypes{
+    Hatchback,Seden,SUV,MUV
+}
+fn printcars(car:CarTypes){
+    match car{
+        CarTypes::Hatchback =>{
+            println!("Hatch car is beast!!")
+        }
+        CarTypes::Seden =>{
+            println!("Seden car is beast!!")
+        }
+        CarTypes::SUV =>{
+            println!("SUV car is beast!!")
+        }
+        CarTypes::MUV =>{
+            println!("MUV cae is beast!!")
+        }
+    }
+}
 fn main() {
     println!("Hello, world!");
-//Error Handling Approaches
-//Approach 1
-enum Option<T>{// Define the generic Option type
-    Some(T), // Represents a value
-    None, // represents no value
-}
-//example is on line 28
+//calling function
+printcars(CarTypes::Hatchback);
 
-match divide(10.0,2.0){
-    Some(x) => println!("Approach 1 Result : {}",x),
-    None => println!("Cannot divide by 0!"),
-}
-
-//Approach 2 : it is like try and catch in javascript
-enum Result<T,E>{//Define the generic Result type
-    Ok(T),//Represent a value
-    Err(E),//Respresent Error
-}
-
-//example is on line 45
-
-match divide_two(100.23,0.0) {
-    Ok(x) => println!("Approach 2 Result :{}",x),
-    Err(err) => println!("Error: {}",err),
-}
-
-}
-//time : 2.35.00
-//exampleon Approach 1
-fn divide(numerator:f64,denominator:f64)->Option<f64>{
-    if denominator == 0.0{
-        None
-    }else{
-        Some(numerator / denominator)
-    }
-}
-
-//Example for Approach 2
-fn divide_two(numerator:f64,denominator:f64)->Result<f64,String>{
-    if denominator == 0.0 {
-        Err("Cannot Divide By 0 !!".to_string())
-    }else{
-        Ok(numerator/denominator)
-    }
 }
