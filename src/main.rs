cc5e1f1ed6d10ff081fae0db73c21b356f5f6e7f
BrightTNut <118769123+BrightTNut@users.noreply.github.com>
2024-11-02 00:03:32 +0530

    advance enum with code with money

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,49 @@
 //For allowing warnings
 #![allow(warnings)]
 
+// Error is :`Person` doesn't implement `Debug`
+// the trait `Debug` is not implemented for `Person`
+// add `#[derive(Debug)]` to `Person` or manually `impl Debug for Person`
+#[derive(Debug)]
+enum GenderCato {
+    Male,
+    Female,
+    Others
+}
 
-enum CarTypes{
-    Hatchback,Seden,SUV,MUV
+#[derive(Debug)]
+struct Person {
+    name:String,
+    email:String,
+    gender:GenderCato
 }
-fn printcars(car:CarTypes){
-    match car{
-        CarTypes::Hatchback =>{
-            println!("Hatch car is beast!!")
-        }
-        CarTypes::Seden =>{
-            println!("Seden car is beast!!")
-        }
-        CarTypes::SUV =>{
-            println!("SUV car is beast!!")
-        }
-        CarTypes::MUV =>{
-            println!("MUV cae is beast!!")
-        }
-    }
-}
+
 fn main() {
-    println!("Hello, world!");
-//calling function
-printcars(CarTypes::Hatchback);
+    let per1 = Person{
+        name:String::from("Pankaj"),
+        email:String::from("kjhkjh@gmail.com"),
+        gender:GenderCato::Male
+    };
+ println!("Person 1 : {:?}",per1);
+ println!("Option enum::");
+ let result = cal(4);
 
+ println!("Number pass in cal fn is even or not : {:?}",result)
+}
+
+
+//Option enum
+// syntax :
+// enum Option<Type>{
+//     Some{Type}
+//     None
+// }
+
+//example of option enum using function whci find even no
+fn cal(no:i32) -> Option<bool>{
+    if(no%2 == 0){
+        Some(true)
+    }else {
+        None
+    }
 }
